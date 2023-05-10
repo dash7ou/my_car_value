56f2a41412023260b93635ce0c40669ae0cfef42
dash7ou <fxfddfno@gmail.com>
2023-05-10 17:34:17 +0300

    save user to database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userServ: UsersService) {}
+
   @Post('/singup')
   createUser(@Body() body: CreateUserDto) {
-    console.log('body', body);
+    this.userServ.create(body.email, body.password);
   }
 }
