03b24c5eca18e013e86f9d3693fa31b4369729a5
dash7ou <fxfddfno@gmail.com>
2023-05-10 18:12:57 +0300

    find and update functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,9 +7,30 @@ import { User } from './users.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  async create(email: string, password: string) {
+  create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  // partial told ts that this object contains at least one or none of User proprity
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found!');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  remove() {}
 }
