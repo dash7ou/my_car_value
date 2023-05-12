2c18756af7db836b14559e92d4e9f4e3cd7ffb0b
dash7ou <fxfddfno@gmail.com>
2023-05-12 15:04:49 +0300

    hash password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,24 +1,31 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private userServ: UsersService) {}
 
-  signup(email: string, password: string){
+  async signup(email: string, password: string) {
     // check email is exist
-    const users = await this.userServ.find(email)
+    const users = await this.userServ.find(email);
 
-    if(users.length > 0){
-        throw new BadRequestException("This email already exist!");
+    if (users.length > 0) {
+      throw new BadRequestException('This email already exist!');
     }
 
     // hash password
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const hashPassword = `${salt}.${hash.toString('hex')}`;
 
     // create new user
 
     // return the user
   }
 
-  signin(){}
+  signin() {}
 }
